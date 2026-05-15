2d2ccfa44e32fb41ec8f85f35b2818d8ab950142
DMiquel <1709948@uab.cat>
2026-05-15 16:44:25 +0200

    data_aumentation_perspective

--- Grup 13/dataloaders.py
+++ Grup 13/dataloaders.py
@@ -27,7 +27,7 @@ PKL_PATH = "/home/edxnG13/grup13/dataset_pickle.pkl"
 # "crop_flip_jitter"
 # "affine_flip_jitter"
 # "crop_flip_jitter_rotation"
-AUGMENTATION_TYPE = "rotation"
+AUGMENTATION_TYPE = "perspective"
 
 # num_workers indica quants processos de CPU carreguen imatges en paral·lel.
 # 4 és un valor bastant habitual: accelera la càrrega sense saturar massa la màquina.
